8225954075aa6df6aecd5745295f305d74c17729
MiguelIbrahimE <miguelibrahim8@gmail.com>
2024-11-22 15:37:20 +0100

    fixing the front page links to the web pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 
-
-ReactDOM.render(
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <BrowserRouter>
         <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </BrowserRouter>
 );
